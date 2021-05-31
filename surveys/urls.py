8a59c8ad03b6f70befdbe55205066b1ab821463d
ju-c <julien.cortesi@protonmail.com>
2021-05-31 13:15:08 +0200

    editing surveys urls

--- surveys/urls.py
+++ surveys/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+        # Homepage
         path('', HomePageView.as_view(), name='home'),
+        # Survey
         path('surveys/', survey_list, name='survey-list'),
-        path('surveys/<int:pk>/edit/', survey_edit , name='survey-edit'),
+        path('surveys/<int:pk>/', survey_detail, name='survey-detail'),
         path('surveys/add/', survey_create, name='survey-add'),
-        path('surveys/<int:pk>/', survey_detail, name= 'survey-detail'),
+        path('surveys/<int:pk>/edit/', survey_edit , name='survey-edit'),
         path('srveys/<int:pk>/delete/', survey_delete, name='survey-delete'),
 ]
