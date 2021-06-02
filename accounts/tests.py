900bbcd0ba60328bb3417dd24a828f6f992180d6
ju-c <julien.cortesi@protonmail.com>
2021-06-02 09:10:45 +0200

    adding tests to accounts/tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
 
 
 class NewUserTests(TestCase):
@@ -30,3 +31,28 @@ class NewUserTests(TestCase):
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
 
+
+class SignupTests(TestCase):
+
+    username = 'julien'
+    email = 'julien@jul1en.com'
+
+    def setUp(self):
+        url = reverse ('account_signup')
+        self.response = self.client.get(url)
+
+    def test_signup_page(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'account/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(
+                self.response, 'Ooops! This is an error!')
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+                self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()
+                        [0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()
+                        [0].email, self.email)
