75188a3f362bc2dc98b687d3b98b9444502a055d
ju-c <julien.cortesi@protonmail.com>
2021-06-02 21:32:45 +0200

    debugging the edit template

--- surveys/forms.py
+++ surveys/forms.py
@@ -22,19 +22,19 @@ class OptionForm(forms.ModelForm):
 
 class AnswerForm(forms.Form):
     def __init__(self, *args, **kwargs):
-        options = kwargs.pop("options")
+        options = kwargs.pop('options')
         choices = {(o.pk, o.choice) for o in options}
         super().__init__(*args, **kwargs)
         option_field = forms.ChoiceField(
                 choices=choices,
                 widget=forms.RadioSelect,
                 required=True)
-        self.fields["option"] = option_field
+        self.fields['option'] = option_field
 
 
 class BaseAnswerFormSet(forms.BaseFormSet):
     def get_form_kwargs(self, index):
         kwargs = super().get_form_kwargs(index)
-        kwargs["options"] = kwargs["options"][index]
+        kwargs['options'] = kwargs['options'][index]
         return kwargs
 
